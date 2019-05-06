e454c82caebbf0671366b82ef881a8f16b458e78
Tair Adibek <adibek.t@maint.kz>
2019-05-06 14:44:54 +0600

    Added models and migrations

--- database/migrations/2019_05_06_081116_create_users_services_table.php
+++ database/migrations/2019_05_06_081116_create_users_services_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsersServicesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_services', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role_id')->default(1);
-            $table->rememberToken();
+            $table->integer('mobile_user_id');
+            $table->integer('service_id');
+            $table->integer('amount');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_services');
     }
 }
--- database/migrations/2019_05_06_083538_create_mobile_users_table.php
+++ database/migrations/2019_05_06_083538_create_mobile_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateMobileUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('mobile_users', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('phone');
             $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('role_id')->default(1);
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -30,8 +26,9 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
+
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mobile_users');
     }
 }
