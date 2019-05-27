69c40ed9d48d5b4c3259eac17c195303b1190e82
Unknown <unknown@example.com>
2019-05-27 09:35:48 +0600

    base 64

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -40,7 +40,7 @@ class ApiController extends Controller
             curl_setopt($ch, CURLOPT_POST, true);
             curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            $server_output = curl_exec($ch);
+           // $server_output = curl_exec($ch);
             curl_close ($ch);
             return $this->makeResponse(200, true, ['message' => 'success']);
         }
@@ -58,8 +58,10 @@ class ApiController extends Controller
             $code = Code::where('phone', $phone)->where('code', $password)->first();
 
             if($code){
-                $token = Str::random(42);
+
                 $user = MobileUser::where('phone', $phone)->first();
+//                $token = Str::random(42);
+                 $token = base64_encode($user);
                 if($user){
                     $user->token = $token;
                 }else{
