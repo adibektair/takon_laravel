e410b3c7da7c9b7889b08f92f17d71b071e4f3d6
Unknown <unknown@example.com>
2019-06-21 11:13:17 +0600

    111

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -676,7 +676,7 @@ class ApiController extends Controller
 
         $paymentModel = new Payment($request->name, $request->cryptogram, $request->ip, $request->amount);
         $response = $paymentModel->pay();
-        return $this->makeResponse(200,true, ['response' => json_encode($response)]);
+        return $this->makeResponse(200,true, ['response' => json_decode($response)]);
 
 
     }
