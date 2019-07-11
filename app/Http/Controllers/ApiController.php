cbef39879947bdb2846bca922a1d6d6f46aa3edf
Unknown <unknown@example.com>
2019-07-11 11:21:34 +0600

    111

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -598,7 +598,7 @@ class ApiController extends Controller
                 ->select('company.name as company', 's_company.name as s_company', 'transactions.*',
                     'services.name as service', 's_users.phone as s_user_phone', 'r_users.phone as r_user_phone',
                     's_users.id as s_user_id', 'r_users.id as r_user_id', 'partners.name as creater', 'return.name as ret_name', 'transactions.type as ttype')
-                ->orderBy('transactions.created_at', 'asc')
+                ->orderBy('transactions.id', 'asc')
                 ->get();
 
                 $result = [];
