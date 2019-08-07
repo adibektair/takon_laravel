62d6237417c9b8a890dbbd454a7a4233b4237e4a
Tair <adibek.t@maint.kz>
2019-08-07 15:52:27 +0600

    zzzz

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -869,7 +869,7 @@ class ApiController extends Controller
             'Authorization: Basic '. base64_encode(self::ID . ":". self::API_KEY)
         ));
         curl_setopt($ch, CURLOPT_URL,"https://api.cloudpayments.kz/payments/tokens/list");
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, 0);
 //        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $server_output = curl_exec ($ch);
