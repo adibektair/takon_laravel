cd83c5dcc307af476068b998d6fd4710ffa1b241
Tair <adibek.t@maint.kz>
2019-08-08 09:30:17 +0600

    online payment DONE

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -588,7 +588,7 @@ class ApiController extends Controller
         $token = $request->token;
         $user = MobileUser::where('token', $token)->first();
         if($user){
-            if($user->phone == "77089995055" OR $user->phone == "77005554797"){
+            if($user->phone == "77005554797"){
                 $result = DB::table('transactions')
                     ->where('transactions.type', 3)
                     ->join('services', 'services.id', '=', 'transactions.service_id')
