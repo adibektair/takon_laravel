978d485931105b782150b791fc2a147bcbf899ec
Tair <adibek.t@maint.kz>
2019-08-12 15:35:07 +0600

    qqwwee

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -183,7 +183,7 @@ class ApiController extends Controller
 //                ->leftJoin('users_services', 'users_services.service_id', '=', 'services.id')
                 ->where('services.status', 3)
 //                ->where('users_services.mobile_user_id', $user->id)
-                ->select('services.id', 'services.price', 'services.name', 'services.created_at','services.description')
+                ->select('services.id', 'services.price', 'services.name', 'services.created_at','services.description', 'services.payment_enabled')
                 ->selectRaw('SUM(DISTINCT users_services.amount) AS usersAmount')
                 ->groupBy('services.id', 'services.price', 'services.name', 'services.created_at', 'services.description', 'services.payment_enabled', 'services.payment_price')
                 ->get();
@@ -207,7 +207,7 @@ class ApiController extends Controller
                 ->where('users_services.service_id', $service_id)
                 ->where('users_services.amount', '<>', 0)
                 ->select('services.*', 'users_services.id', 'users_services.amount as usersAmount')
-                ->selectRaw('IFNULL(companies_services.deadline, 1577880000) AS deadline, IFNULL(companies.name, "") as company')
+                ->selectRaw('IFNULL(companies_services.deadline, 1577880000) AS deadline, IFNULL(companies.name, "TAKON.ORG") as company')
                 ->distinct('users_services.id')
                 ->groupBy('users_services.id')
                 ->get();
