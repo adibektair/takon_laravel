731722906aca0191ac1d60293ddc5b776fc96387
Tair <adibek.t@maint.kz>
2019-09-11 16:46:21 +0600

    Watch gas created

--- app/Console/Commands/WatchGasHistory.php
+++ app/Console/Commands/WatchGasHistory.php
@@ -2,6 +2,8 @@
 
 namespace App\Console\Commands;
 
+use HttpException;
+use HttpRequest;
 use Illuminate\Console\Command;
 
 class WatchGasHistory extends Command
@@ -38,25 +40,51 @@ class WatchGasHistory extends Command
     public function handle()
     {
 
-        $data = array("login" => "K770217301057", "password" => "Qwer1234", "submit" => "Войти", "is_post" => 1);
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Content-Type: application/x-www-form-urlencoded',
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://lk.scanoil.ru/login",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "is_post=1&login=K770217301057&password=Qwer1234&submit=%D0%92%D0%BE%D0%B9%D1%82%D0%B8",
+            CURLOPT_HTTPHEADER => array(
+                "Accept: */*",
+                "Accept-Encoding: gzip, deflate",
+                "Cache-Control: no-cache",
+                "Connection: keep-alive",
+                "Content-Type: application/x-www-form-urlencoded",
+                "Cookie: session=ka5k40njfo8dbcrnebbvkvl800",
+                "Postman-Token: bbb884ee-b5ad-4bba-8fb2-2c55881acbf2,1b5bcacb-0eae-41eb-9fee-3a7945afc679",
+                "Referer: http://lk.scanoil.ru/cabinet",
+                "User-Agent: PostmanRuntime/7.16.3",
+                "cache-control: no-cache"
+            ),
         ));
-        curl_setopt($ch, CURLOPT_URL,"https://lk.scanoil.ru/login");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $server_output = curl_exec ($ch);
-        curl_close ($ch);
-        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $server_output, $matches);
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            echo $response;
+        }
+
+
+        preg_match_all('/^Set-Cookie:\s*([^;]*)/mi', $response, $matches);
         $cookies = array();
         foreach($matches[1] as $item) {
             parse_str($item, $cookie);
             $cookies = array_merge($cookies, $cookie);
         }
         var_dump($cookies);
+
     }
 }
