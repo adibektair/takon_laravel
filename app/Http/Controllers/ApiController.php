00f3bebb295fc1e9576ca10b08360fa36c99d153
Tair <adibek.t@maint.kz>
2019-10-22 13:06:50 +0600

    $el['contragent'] = $partner->name . " (" . $suser->name . ")";

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -1072,7 +1072,12 @@ class ApiController extends Controller
                             if ($value->ttype == 3) {
                                 $suser = User::where('id', $value->r_user_id)->first();
                                 $partner = Partner::where('id', $suser->partner_id)->first();
-                                $el['contragent'] = $partner->name . " (" . $suser->name . ")";
+                                if($suser->name){
+                                    $el['contragent'] = $partner->name . " (" . $suser->name . ")";
+                                }else{
+                                    $el['contragent'] = $partner->name;
+                                }
+
                             } else {
                                 $el['contragent'] = $value->r_user_phone;
                             }
