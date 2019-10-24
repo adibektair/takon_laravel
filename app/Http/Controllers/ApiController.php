7dc72ab958a655662e8cedcafcaa6d0d3b7416e5
Tair <adibek.t@maint.kz>
2019-10-24 09:27:41 +0600

    api change

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -203,10 +203,10 @@ class ApiController extends Controller
                         ->where('users_services.mobile_user_id', $user->id);
                 })
 //                ->leftJoin('users_services', 'users_services.service_id', '=', 'services.id')
-                ->where('services.status', 3)
+//                ->where('services.status', 3)
 //                ->where('users_services.mobile_user_id', $user->id)
                 ->select('services.id', 'services.price', 'services.name', 'services.created_at', 'services.description', 'services.payment_enabled', 'services.payment_price')
-                ->selectRaw('SUM(DISTINCT users_services.amount) AS usersAmount')
+                ->selectRaw('SUM(users_services.amount) AS usersAmount')
                 ->groupBy('services.id', 'services.price', 'services.name', 'services.created_at', 'services.description', 'services.payment_enabled', 'services.payment_price')
                 ->get();
 
