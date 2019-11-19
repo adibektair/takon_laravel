70c0458dc2e4d0f294faa91ceefd424fac12b21d
Tair <adibek.t@maint.kz>
2019-11-19 18:38:49 +0600

    commit

--- resources/views/wallet.blade.php
+++ resources/views/wallet.blade.php
@@ -12,7 +12,7 @@ $wallet_payment = \App\WalletPayment::where('id', $id)->first();
 	  <input name="WMI_MERCHANT_ID"    value="170961124637"/>
   <input name="WMI_PAYMENT_AMOUNT" value="<?=$wallet_payment->prices?>.00"/>
   <input name="WMI_CURRENCY_ID"    value="398"/>
-  <input name="WMI_DESCRIPTION"    value="Покупка <?= $wallet_payment->amount ?> Таконов  <?= $wallet_payment->getService()->name?> "/>
+  <input name="WMI_DESCRIPTION"    value="Покупка <?= $wallet_payment->amount ?> Таконов"/>
   <input name="WMI_SUCCESS_URL"    value="https://takon.org/wallet-success"/>
   <input name="WMI_FAIL_URL"       value="https://takon.org/wallet-fail"/>
   <input name="WMI_PAYMENT_NO"       value="<?=$wallet_payment->id?>"/>
