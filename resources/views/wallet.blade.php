2f34865b1719da1734769520e4f8eb33a49c5ffd
Tair <adibek.t@maint.kz>
2019-11-19 18:40:37 +0600

    commit

--- resources/views/wallet.blade.php
+++ resources/views/wallet.blade.php
@@ -10,7 +10,7 @@ $wallet_payment = \App\WalletPayment::where('id', $id)->first();
 
 <form method="post" name="downloadForm" action="https://wl.walletone.com/checkout/checkout/Index">
 	  <input name="WMI_MERCHANT_ID"    value="170961124637"/>
-  <input name="WMI_PAYMENT_AMOUNT" value="<?=$wallet_payment->prices?>.00"/>
+  <input name="WMI_PAYMENT_AMOUNT" value="<?=$wallet_payment->price?>.00"/>
   <input name="WMI_CURRENCY_ID"    value="398"/>
   <input name="WMI_DESCRIPTION"    value="Покупка <?= $wallet_payment->amount ?> Таконов"/>
   <input name="WMI_SUCCESS_URL"    value="https://takon.org/wallet-success"/>
