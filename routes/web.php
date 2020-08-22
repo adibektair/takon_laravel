35c8a6b30cdb68d5e16b6a57465505e8066f0bcf
tair <adibek.t@maint.kz>
2020-08-22 18:43:48 +0600

    New updated welcome page

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/secure/config/key-generate', ['uses' => 'ConfigController@keyGenera
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('intro');
 });
 Route::get('/payment', function () {
     return view('mobile_users/payment');
