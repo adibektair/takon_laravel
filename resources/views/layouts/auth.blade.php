25fc445ab4be72e568c31a5536d76c298fc531eb
tair <adibek.t@maint.kz>
2020-08-23 22:34:39 +0600

    Modifying Login Page

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -8,13 +8,14 @@
     <title>Takon</title>
 
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
     <link href="{{asset('admin/bower_components/bootstrap/dist/css/bootstrap.css')}}" rel="stylesheet">
     <link href="{{asset('css/popModal.min.css')}}" rel="stylesheet">
     <link href="{{asset('css/popModal.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/styleAuth.css') }}">
 
     <!-- Custom styles for this template -->
     <link href="{{asset('css/simple-sidebar.css')}}" rel="stylesheet">
@@ -28,10 +29,10 @@
     </style>
 </head>
 <body id="app-layout">
-<nav class="navbar navbar-dark bg-dark mb-4">
+<nav class="navbar navbar-dark bg-dark mb-4 ">
     <span class="navbar-brand mb-0 h1">
-        <a class="navbar-brand" href="{{ url('/') }}">
-                Takon
+        <a class="navbar-brand formatted__text" href="{{ url('/') }}">
+                TAKON
             </a>
     </span>
 </nav>
