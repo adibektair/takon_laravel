cffdd75b26dca2b85e3579692b21f1a4b207cf88
tair <adibek.t@maint.kz>
2020-08-27 12:01:52 +0600

    Removing link android-chrome

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -9,7 +9,6 @@
 
     <!-- Fav icon -->
     <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" sizes="180x180" href="/favicon/android-chrome-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
     <link rel="manifest" href="/favicon/site.webmanifest">
