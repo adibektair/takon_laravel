1a2ff4ec6afa759e513d85adaebb6f8c4f65f877
tair <adibek.t@maint.kz>
2020-08-27 21:53:55 +0600

    Updating favicon icons

--- resources/views/intro.blade.php
+++ resources/views/intro.blade.php
@@ -8,12 +8,15 @@
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
     <!-- Fav icon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/favicon/site.webmanifest">
-    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#161a1b">
-    <meta name="msapplication-TileColor" content="#160734">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png?v=9B0a385Eeb">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png?v=9B0a385Eeb">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png?v=9B0a385Eeb">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png?v=9B0a385Eeb">
+    <link rel="manifest" href="/favicon/site.webmanifest?v=9B0a385Eeb">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?v=9B0a385Eeb" color="#091316">
+    <link rel="shortcut icon" href="/favicon/favicon.ico?v=9B0a385Eeb">
+    <meta name="msapplication-TileColor" content="#2b5797">
+    <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png?v=9B0a385Eeb">
     <meta name="theme-color" content="#ffffff">
 
     <title>Takon</title>
