13680c5e51be478df7380b658fe0dde824d0aac6
tair <adibek.t@maint.kz>
2020-09-09 23:18:40 +0600

    Mail send reset password link

--- config/mail.php
+++ config/mail.php
@@ -145,10 +145,10 @@ return [
     'driver' => env('MAIL_DRIVER', 'smtp'),
     'host' => env('MAIL_HOST', 'smtp.gmail.com'),
     'port' => env('MAIL_PORT', 587),
-    'from' => ['address' => 'adibek.tair@gmail.com', 'name' => 'Takon App'],
+    'from' => ['address' => 'noreplytakon@gmail.com', 'name' => 'Takon'],
     'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-    'username' => env('adibek.tair@gmail.com'),
-    'password' => env('Rossoneri69'),
+    'username' => env('MAIL_USERNAME'),
+    'password' => env('MAIL_PASSWORD'),
     'sendmail' => '/usr/sbin/sendmail -bs',
     'pretend' => false,
 ];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header">{{ __('Логин') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Пароль') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -45,7 +45,7 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Запомнить') }}
                                     </label>
                                 </div>
                             </div>
@@ -54,15 +54,13 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    {{ __('Войти') }}
                                 </button>
                             </div>
                             <div class="col-md-6 offset-md-4 link">
-                                @if (Route::has('password.request'))
-                                    <a href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
+                                 <a href="{{ route('password.request') }}">
+                                     {{ __('Забыли пароль?') }}
+                                 </a>
                             </div>
                         </div>
                     </form>
